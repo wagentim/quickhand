1729a6c679ce5368766b2d290aacdf5b903fcb0e
wagentim <bhwerbung@gmail.com>
2015-10-30 17:40:37 +0100

    update

---
+++ quickhand/src/cn/wagentim/parser/IParserConstants.java
@@ -0,0 +1,7 @@
+package cn.wagentim.parser;
+
+public interface IParserConstants
+{
+	public static final int LINK = 0;
+	public static final int TEXT = 1;
+}
--- quickhand/src/cn/wagentim/parser/Parser.java
+++ quickhand/src/cn/wagentim/parser/Parser.java
@@ -2,6 +2,7 @@ package cn.wagentim.parser;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.xml.parsers.ParserConfigurationException;
@@ -10,13 +11,17 @@ import javax.xml.parsers.SAXParserFactory;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
 import cn.wagentim.connection.GetPageContent;
 
-public final class Parser
+public final class Parser implements IParserConstants
 {
 	private final Logger logger = LogManager.getLogger(GetPageContent.class);
 	
@@ -40,7 +45,9 @@ public final class Parser
 			
 			try
 			{
-				processFile(file, new ContentHandler());
+				GetPageContent pageGetter = new GetPageContent();
+				
+				processFile(file, new ContentHandler(pageGetter) );
 			}
 			catch (ParserConfigurationException | SAXException e)
 			{
@@ -64,12 +71,86 @@ public final class Parser
 	
 	private class ContentHandler extends DefaultHandler
 	{
+		private final GetPageContent pageGetter;
+		private String pageContent;
+		private List<String> urls;
+		private List<String> results;
+		private int currentPageLevel = 0;
+		private Document doc = null;
+		private Element findElement = null;
+		private int returnType = -1;
+		
+		public ContentHandler(final GetPageContent pageGetter)
+		{
+			this.pageGetter = pageGetter;
+			urls = new ArrayList<String>();
+			results = new ArrayList<String>();
+		}
+		
 		public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException
 		{
 			if( qName.equals("site") )
 			{
 				// new site start
 				logger.error("process the site...");
+				pageGetter.run(localName);
+				this.pageContent = pageGetter.getPageContent();
+			}
+			else if( qName.equals("page") )
+			{
+				currentPageLevel = Integer.parseInt(localName);
+				doc = Jsoup.parse(pageContent);
+			}
+			else if( qName.equals("parser") )
+			{
+				Elements es = doc.select(localName);
+				if( es.size() == 0 )
+				{
+					logger.error("Cannot find elements with the selection code: " + localName);
+					return;
+				}
+				else
+				{
+					if( es.size() > 1 )
+					{
+						logger.error("Find more than one elements with the selection code: " + localName);
+					}
+					
+					findElement = es.get(0);
+				}
+			}
+			else if( qName.equals("return") )
+			{
+				returnType = Integer.parseInt(localName);
+			}
+		}
+		
+		public void endElement(String uri, String localName, String qName) throws SAXException
+		{
+			if( qName.equals("block") )
+			{
+				if( LINK == returnType )
+				{
+					urls.add(findElement.text());
+				}
+				else if( TEXT == returnType )
+				{
+					results.add(findElement.text());
+				}
+			}
+			else if( qName.equals("site") )
+			{
+				printResults("##############  Results  ##############", results);
+				printResults("##############  Links  ##############", urls);
+			}
+		}
+
+		private void printResults(String title, List<String> contents)
+		{
+			System.out.println(title);
+			for( int i = 0;  i < contents.size(); i++ )
+			{
+				System.out.println(contents.get(i));
 			}
 		}
 	}
